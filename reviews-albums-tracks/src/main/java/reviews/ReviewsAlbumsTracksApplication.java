21c2a783407aff89fcb490e06ba3ad38d1c919e0
jmalue <jrod2416@gmail.com>
2020-02-24 18:59:13 -0500

    Commiting

---
+++ reviews-albums-tracks/src/main/java/reviews/ReviewsAlbumsTracksApplication.java
@@ -0,0 +1,13 @@
+package reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsAlbumsTracksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsAlbumsTracksApplication.class, args);
+	}
+
+}
---
+++ reviews-albums-tracks/src/test/java/reviews/ReviewsAlbumsTracksApplicationTests.java
@@ -0,0 +1,13 @@
+package reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsAlbumsTracksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
